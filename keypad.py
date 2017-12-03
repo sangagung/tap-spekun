f29c55ee307ce48b6e5a1ccacf5c64a9e6143a7e
Rizqy Faishal <rizqyfaishal27@gmail.com>
2017-12-03 15:27:12 +0700

    update

---
+++ keypad.py
@@ -0,0 +1,28 @@
+from pad4pi import rpi_gpio
+import time
+
+KEYPAD = [
+    [1, 2, 3, "A"],
+    [4, 5, 6, "B"],
+    [7, 8, 9, "C"],
+    ["*", 0, "#", "D"]
+]
+
+ROW_PINS = [4, 14, 15, 17] # BCM numbering
+COL_PINS = [18, 21, 22, 23] # BCM numbering
+
+factory = rpi_gpio.KeypadFactory()
+
+keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PINS)
+
+def printKey(key):
+    print(key)
+
+# printKey will be called each time a keypad button is pressed
+keypad.registerKeyPressHandler(printKey)
+
+while True:
+	time.sleep(0.7)
+	pass
+
+keypad.cleanup()
